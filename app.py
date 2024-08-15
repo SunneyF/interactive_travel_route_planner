4c7d0427d3e4ad8e3328fd56edfe5162c54e94fb
Sunney Fotedar <sunney219@gmail.com>
2024-08-16 00:26:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,16 @@ from geopy.distance import geodesic
 import requests
 import random
 import math
-
+from PIL import Image
 import streamlit as st
 
 def main():
     st.title('Hello, Heroku!')
     st.write('This is a simple Streamlit app deployed on Heroku.')
+    image = Image.open('survey.jpg')
+    col1.image(image,
+            caption='Designed by slidesgo / Freepik',
+            use_column_width=True)
 
 if __name__ == "__main__":
     main()
